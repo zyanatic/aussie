8f29de193a1ba1c5d3ff9d30642fdd704b471b6d
zyanatic <34130257+zyanatic@users.noreply.github.com>
2018-05-10 08:32:47 +0200

    Update aussiepanda.py

--- aussiepanda.py
+++ aussiepanda.py
@@ -8,16 +8,18 @@ import os
 
 playerlist = "D:/players.txt"
 
-page = requests.get("http://websites.sportstg.com/round_info.cgi?c=1-10178-151184-391992-24906259&pool=&fixture=65922965&a=SELECT")
+page = requests.get("http://websites.sportstg.com/round_info.cgi?c=1-10178-151184-478257-0&pool=1&fixture=69449576&a=SELECT")
 tree = page.content
 soup = BeautifulSoup(tree,"html5lib")
 
 # Get the URLs for home and away teams, later needed to construct URLs for each STATS page
-# This can possibly be improved for flexibility
-team_info = soup.find_all(href=re.compile("team_info"))
-home_url = team_info[2].get('href')
-away_url = team_info[4].get('href')
-
+home_logo = soup.findAll('div', {'class' : 'hometeam'})
+for div in home_logo:
+    home_url = "http://websites.sportstg.com/" + div.find('a')['href']
+away_logo = soup.findAll('div', {'class' : 'awayteam'})
+for div in away_logo:
+    away_url = "http://websites.sportstg.com/" + div.find('a')['href']
+print(home_url)
 # Get the aID / assocID
 # For games between teams from different associations (e.g. cup, friendly) we will have to see if we can get each teams aID separately
 aid = soup.find(id="aid").get_text()
@@ -39,30 +41,45 @@ def get_playerSWW():
 	tree = page.content
 
 	data = json.loads(tree)
-	urls = []
+	home_starting = []
+	home_subs = []
+	away_starting = []
+	away_subs = []
 	sww = []
+	x = -1
 
-# Construct the URLs we need to navigate to
+# Construct the URLs we need to navigate to. starting and subs URLs are the same for each team, but it makes sense to keep them
+# separated for later
 	for pid in data['PlayersPosition']:
-		# Here is where we need aID (same as assocID) again
-		urls.append("http://websites.sportstg.com/aj_swwid.cgi?playerID="+pid[2]+"&assocID="+aid)
+		x = x + 1
+		if "Home" in str(data['PlayersPosition'][x][0]) and "Sub" not in str(data['PlayersPosition'][x][0]):
+			home_starting.append("http://websites.sportstg.com/aj_swwid.cgi?playerID="+pid[2]+"&assocID="+aid)
+		elif "Home" in str(data['PlayersPosition'][x][0]) and "Sub" in str(data['PlayersPosition'][x][0]):
+			home_subs.append("http://websites.sportstg.com/aj_swwid.cgi?playerID="+pid[2]+"&assocID="+aid)
+		elif "Away" in str(data['PlayersPosition'][x][0]) and "Sub" not in str(data['PlayersPosition'][x][0]):
+			away_starting.append("http://websites.sportstg.com/aj_swwid.cgi?playerID="+pid[2]+"&assocID="+aid)
+		elif "Away" in str(data['PlayersPosition'][x][0]) and "Sub" in str(data['PlayersPosition'][x][0]):
+			away_subs.append("http://websites.sportstg.com/aj_swwid.cgi?playerID="+pid[2]+"&assocID="+aid)
 
-# Visit each URL and grab the swwPlayerID and write them to a text file
-	for link in urls:
-		page = requests.get(link)
-		tree = page.content
-		data = json.loads(tree)
-		sww.append(data['swwPlayerID'])
-		with open(playerlist, "a") as csvfile:	
-			print(data['swwPlayerID'], file=csvfile)
+# Visit each URL and grab the swwPlayerID and write them to a text file - This is probably useless, we need to pull all
+# swwPlayerIDs anyway, as lineups always change, only pulling them once and saving to text file does not help. 
+# Instead of just appending all IDs to one list, we need to do this for home_starting, home_subs, etc.
+#
+#	for link in home_url:
+#		page = requests.get(link)
+#		tree = page.content
+#		data = json.loads(tree)
+#		sww.append(data['swwPlayerID'])
+#		with open(playerlist, "w") as csvfile:	
+#			print(data['swwPlayerID'], file=csvfile)
 	return;
 
-# Checks if txt file with sww player ids already exists, if it's not there, executes the function
-if os.path.isfile(playerlist):
-	print("Player list already exists")
-else:
-	get_playerSWW()
-
+# Checks if txt file with sww player ids already exists, if it's not there, executes the function. See above, probably not needed.
+#if os.path.isfile(playerlist):
+#	print("Player list already exists")
+#else:
+#	get_playerSWW()
+get_playerSWW()
 
 # This snippet should get ALL tables on the page, each table can be selected by index tables[0] etc.
 def get_tables():
